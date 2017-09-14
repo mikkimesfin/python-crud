0d62bd3d035d2c898e245afe9178db97e51a6d01
Mikki Mesfin <mikki.mesfin@gmail.com>
2017-09-14 09:37:44 -0600

    create the model

--- flaskapp/main.py
+++ flaskapp/main.py
@@ -8,3 +8,12 @@ basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' +
 os.path.join(basedir, 'app.sqlite')
 db = SQLAlchemy(app)
+
+class Note(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(80))
+    body = db.Column(db.Text)
+
+    def __init__(self, title, body):
+        self.title = title
+        self.body = body
